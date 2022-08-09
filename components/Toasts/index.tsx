0f29299f85870f4f40294f38e707a6403c8a6095
wtt478478@163.com <wtt478478@163.com>
2022-08-09 21:21:46 +0800

    引入修改

--- components/Toasts/index.tsx
+++ components/Toasts/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "classnames";
 import Notification from "./Notification";
 import styles from "./toast.module.scss";
-import Image from "next/Image";
+
 // Toast组件比较特殊
 // 由于<Toast />不会被直接渲染在DOM中
 // 而是动态插入页面中
